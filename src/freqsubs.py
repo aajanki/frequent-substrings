4c0db5c1d09049afff855efc5aabf10645c022c5
Antti Ajanki <antti.ajanki@iki.fi>
2020-12-30 17:25:17 +0200

    Frequent substrings

--- src/freqsubs.py
+++ src/freqsubs.py
@@ -1,8 +1,35 @@
-from suffix_trees import STree
+from suffix_trees import STree  # type: ignore
+from typing import Iterable, Tuple
 
 
-def substring_frequencies(text):
-    """Returns all substrings of text and their frequencies.
+def find_frequent_substrings(
+        text: str,
+        min_support: int,
+        min_length: int = 1
+) -> Iterable[Tuple[str, int]]:
+    """Find frequent substrings of text.
+
+    Returns an unsorted iterable of (substring, frequency) tuples. The
+    output contains substrings that occur at least min_support times
+    and are at least min_length characters long. The output includes
+    only the longest possible substrings, that is no substring in the
+    output is a prefix of another.
+
+    The runtime and memory requirement are linear O(n), where n is the
+    length of the input text.
+    """
+    if text == '':
+        return []
+
+    tree = STree.STree(text)
+    for (prefix, suffix, freq) in _find_substrings(tree, min_support, only_dominating_substrings=True):
+        text = prefix + suffix
+        if len(text) >= min_length:
+            yield (text, freq)
+
+
+def find_substrings(text: str) -> Iterable[Tuple[str, int]]:
+    """Find all substrings of text and their frequencies.
 
     Returns an unsorted iterable of (substring, frequency) tuples. The
     frequencies include all overlapping occurrences of a substring.
@@ -18,7 +45,11 @@ def substring_frequencies(text):
             yield (prefix + s, freq)
 
 
-def _find_substrings(tree):
+def _find_substrings(
+        tree: STree.STree,
+        min_support: int = 1,
+        only_dominating_substrings: bool = False
+) -> Iterable[Tuple[str, str, int]]:
     root = tree.root
     # Stack: (node, tree level, is pre step)
     stack = [(root, 0, True)]
@@ -30,6 +61,10 @@ def _find_substrings(tree):
     # path_label[i] is the label on the i:th edge along the path from
     # the root to the current node.
     path_labels = []
+    # child_has_yielded[i] is True if we have already returned a
+    # substring from any child node below tree depth i. Useful when
+    # returnign only dominating substrings.
+    child_has_yielded = []
     prev_level = -1
 
     while stack:
@@ -49,15 +84,17 @@ def _find_substrings(tree):
                 )
                 freq_acc.append(0)
                 path_labels.append(edge_label)
+                child_has_yielded.append(False)
 
         else:
             # post-step: all children have been processed and we have
             # the frequency. Yield current node's label and frequency.
-
             freq = 0
+            dominated = False
             if level < prev_level:
                 freq = freq_acc.pop()
                 path_labels.pop()
+                dominated = child_has_yielded.pop() and only_dominating_substrings
             elif node.is_leaf():
                 freq = 1
 
@@ -67,24 +104,46 @@ def _find_substrings(tree):
             freq_acc[-1] += freq
 
             text = ''.join(path_labels)
-            if text or edge_label:
+            if freq >= min_support and not dominated and edge_label:
+                child_has_yielded = [True for _ in child_has_yielded]
                 yield (text, edge_label, freq)
 
             prev_level = level
 
 
-def _is_terminator(c):
+def _is_terminator(c: str) -> bool:
     i = ord(c)
     return (i in range(0xE000, 0xF8FF+1) or
             i in range(0xF0000, 0xFFFFD+1) or
             i in range(0x100000, 0x10FFFD+1))
 
 
-def substring_frequencies_slow(text):
+def find_frequent_substrings_slow(text: str, min_support: int, min_length: int = 1) -> Iterable[Tuple[str, int]]:
+    """Find frequent substrings of text.
+
+    Like find_frequent_substrings() but asymptotically slower O(n^2).
+    """
+    substrings = (
+        x for x in find_substrings_slow(text)
+        if len(x[0]) >= min_length and x[1] >= min_support)
+    substrings_sorted = sorted(substrings)
+
+    frequent = []
+    for candidate, next_candidate in zip(substrings_sorted[:-1], substrings_sorted[1:]):
+        if not next_candidate[0].startswith(candidate[0]):
+            frequent.append(candidate)
+
+    if substrings_sorted:
+        frequent.append(substrings_sorted[-1])
+
+    return frequent
+
+
+def find_substrings_slow(text: str) -> Iterable[Tuple[str, int]]:
     """Get all substrings of text and their frequencies.
 
-    The output equals to substring_frequencies() but this is
-    asymptotically slower and uses more memory.
+    The output equals to find_substrings() but this is asymptotically
+    slower and uses more memory.
     """
     substrings = set(text[i:j]
                      for i in range(len(text))
@@ -92,12 +151,12 @@ def substring_frequencies_slow(text):
     return ((s, count_overlapping(text, s)) for s in substrings)
 
 
-def prefixes(s):
+def prefixes(s: str) -> Iterable[str]:
     for i in range(1, len(s) + 1):
         yield s[:i]
 
 
-def count_overlapping(text, sub):
+def count_overlapping(text: str, sub: str) -> int:
     """The number of overlapping occurrences of the substring sub in text."""
     count = 0
     start = 0
--- tests/test_freqsubs.py
+++ tests/test_freqsubs.py
@@ -1,12 +1,16 @@
-from src.freqsubs import substring_frequencies, substring_frequencies_slow
+import pytest
+from src.freqsubs import find_substrings, find_substrings_slow, \
+    find_frequent_substrings, find_frequent_substrings_slow
+
+doppler_text = "Doppler spectroscopy (also known as the radial-velocity method, or colloquially, the wobble method) is an indirect method for finding extrasolar planets and brown dwarfs from radial-velocity measurements via observation of Doppler shifts in the spectrum of the planet's parent star."
 
 
 def test_empty():
-    assert list(substring_frequencies('')) == []
+    assert list(find_substrings('')) == []
 
 
 def test_one():
-    assert list(substring_frequencies('A')) == [('A', 1)]
+    assert list(find_substrings('A')) == [('A', 1)]
 
 
 def test_bbc():
@@ -18,7 +22,7 @@ def test_bbc():
         ('C', 1)
     ]
 
-    assert sorted(substring_frequencies('BBC')) == bbc_substrings
+    assert sorted(find_substrings('BBC')) == bbc_substrings
 
 
 def test_banana():
@@ -40,11 +44,11 @@ def test_banana():
         ('nana', 1)
     ]
 
-    assert sorted(substring_frequencies('banana')) == banana_substrings
+    assert sorted(find_substrings('banana')) == banana_substrings
 
 
 def test_nefernefernefer():
-    substrings = list(substring_frequencies('nefernefernefer'))
+    substrings = list(find_substrings('nefernefernefer'))
 
     assert ('e', 6) in substrings
     assert ('n', 3) in substrings
@@ -52,7 +56,43 @@ def test_nefernefernefer():
     assert ('ern', 2) in substrings
 
 
-def test_longer_text():
-    text = "Doppler spectroscopy (also known as the radial-velocity method, or colloquially, the wobble method) is an indirect method for finding extrasolar planets and brown dwarfs from radial-velocity measurements via observation of Doppler shifts in the spectrum of the planet's parent star."
+def test_substrings_doppler():
+    assert sorted(find_substrings(doppler_text)) == sorted(find_substrings_slow(doppler_text))
 
-    assert sorted(substring_frequencies(text)) == sorted(substring_frequencies_slow(text))
+
+def test_frequent_empty():
+    assert list(find_frequent_substrings('', 1)) == []
+
+
+def test_frequent_one():
+    assert list(find_frequent_substrings('A', 1)) == [('A', 1)]
+    assert list(find_frequent_substrings('A', 2)) == []
+    assert list(find_frequent_substrings('A', 3)) == []
+
+
+def test_frequent_banana():
+    banana_1 = [
+        ('anana', 1),
+        ('banana', 1),
+        ('nana', 1)
+    ]
+    banana_2 = [
+        ('ana', 2),
+        ('na', 2)
+    ]
+    banana_3 = [
+        ('a', 3)
+    ]
+
+    assert sorted(find_frequent_substrings('banana', 1)) == banana_1
+    assert sorted(find_frequent_substrings('banana', 2)) == banana_2
+    assert sorted(find_frequent_substrings('banana', 3)) == banana_3
+    assert sorted(find_frequent_substrings('banana', 4)) == []
+
+
+@pytest.mark.parametrize("min_support,min_length",
+                         [(1, 1), (2, 1), (4, 1), (39, 1), (40, 1),
+                          (2, 19), (2, 20), (3, 6), (3, 8)])
+def test_frequent_doppler(min_support, min_length):
+    assert sorted(find_frequent_substrings(doppler_text, min_support, min_length)) == \
+        sorted(find_frequent_substrings_slow(doppler_text, min_support, min_length))
